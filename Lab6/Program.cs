2a85d17ef3732055439846d095d7413f35707fb8
kovalsofiia <koval.sofiia@student.uzhnu.edu.ua>
2023-11-23 21:23:11 +0200

    lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        //        Використовуючи SqlCommand підготувати програмну оболонку для виконання завдань лабораторної роботи 5. 
+        //Забезпечити користувачу можливість ввести значення параметрів запиту.
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=usersdata.db"))
+            {
+                connection.Open();
+            }
+            Console.Read();
+        }
+    }
+}
